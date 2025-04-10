08643173dad3c6695ba4fbdb411409b23feafea4
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-10 13:48:54 +0300

    adding validations

--- src/components/Password.js
+++ src/components/Password.js
@@ -6,7 +6,16 @@ const PasswordChecker = () => {
         <div className="container">
             <div className="card">
                 <div className="card-header">
-                    <h2>Password Strength Checker</h2>
+                    <h2 className="title">Password Strength Checker</h2>
+                </div>
+
+
+                <div className="card-body">
+                    <div className="input-container">
+                        <div className="input-box">
+                            <input value={password}></input>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
