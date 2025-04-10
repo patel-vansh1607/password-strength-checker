279be5da3b4fc842501da3e38eca0fb8df775d15
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-10 13:43:46 +0300

    adding divs and h1

--- src/components/Password.js
+++ src/components/Password.js
@@ -1,2 +1,14 @@
 import "../styles/Password.css"
 import React, { useState } from "react";
+
+const PasswordChecker = () => {
+    return(
+        <div className="container">
+            <div className="card">
+                <div className="card-header">
+                    <h2>Password Strength Checker</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
